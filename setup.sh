21d50bbee71390e5af38c39fe257f2fb2ffece2d
rakshith-kumar-j <rakshithkumarj@gmail.com>
2020-08-19 12:04:52 +0530

    Added JQuery

---
+++ setup.sh
@@ -0,0 +1,7 @@
+npm init
+npm install lite-server --save
+npm install jquery@3.1.1 --save 
+npm install popper.js@1.12.9 --save 
+npm install bootstrap@4.0.0 --save 
+npm install font-awesome@4.7.0 --save 
+npm install bootstrap-social@5.1.1 --save
